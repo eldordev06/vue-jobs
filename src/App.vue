d8a95515e209aec3b5668165146db813bf2b2085
Eldor <eldorforprogramming@gmail.com>
2024-08-10 17:19:33 +0500

    JobListing & JobListings components completed

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
 import HomeCards from "@/components/HomeCards.vue";
+import JobsListings from "@/components/JobsListings.vue";
 </script>
 
 <template>
     <Navbar />
     <Hero />
     <HomeCards />
+    <JobsListings :limit="3" :show-btn="true" />
 </template>
 
 <style scoped></style>
