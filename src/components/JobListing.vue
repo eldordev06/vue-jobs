f145d453097972fab3a50a1f883e81be1bba820b
Eldor <eldorforprogramming@gmail.com>
2024-08-15 17:10:03 +0500

    JobView.vue created & json server installed and initiated

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -41,7 +41,7 @@ function toggleFullDescription() {
                     <i class="pi pi-map-marker text-inherit"></i>
                     {{ job.location }}
                 </div>
-                <RouterLink :to="'/job/' + job.id"
+                <RouterLink :to="'/jobs/' + job.id"
                             class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm">
                     Read More
                 </RouterLink>
