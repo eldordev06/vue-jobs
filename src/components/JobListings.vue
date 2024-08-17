fef8f57c10311a066393ff85246bb59207a51461
Eldor <eldorforprogramming@gmail.com>
2024-08-17 22:07:11 +0500

    vue-spinner installed & applied

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -3,6 +3,7 @@ import { reactive, onMounted } from "vue";
 import JobListing from "./JobListing.vue";
 import { RouterLink } from "vue-router";
 import axios from "axios";
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 
 const state = reactive({
     jobs: [],
@@ -33,7 +34,12 @@ onMounted(async () => {
     <section class="bg-blue-50 px-4 py-10">
         <div class="container-xl lg:container m-auto">
             <h1 class="text-3xl font-bold text-green-500 mb-6 text-center capitalize">browse jobs</h1>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <div v-if="state.isLoading"
+                 class="text-center text-gray-500 py-6">
+                <PulseLoader />
+            </div>
+            <div v-else
+                 class="grid grid-cols-1 md:grid-cols-3 gap-6">
                 <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
                             :key="job.id"
                             :job="job" />
